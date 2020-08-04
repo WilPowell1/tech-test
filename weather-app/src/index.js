063525243c996710df00c6c60187c818b6f34e2f
WilPowell1 <powellwil88@gmail.com>
2020-08-04 16:15:50 +0100

    location-details test

---
+++ weather-app/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/app';
+import { location } from './data/forecast.json';
+
+render( < App location = { location }
+        />, document.getElementById('root'));
