7c9aa4b6866ac240bdf43acc9e07ef348d8f24af
WilPowell1 <powellwil88@gmail.com>
2020-08-05 16:24:57 +0100

    first component

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -2,18 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LocationDetails from './components/location-details';
+import PropTypes from 'prop-types';
 
-const App = props => ( <
-    LocationDetails city = { props.location.city }
-    country = { props.location.country }
-    />
-);
+const App = props => 
+  <LocationDetails 
+    city={props.location.city}
+    country={props.location.country}
+  />
 
 App.propTypes = {
-    location: PropTypes.shape({
-        city: PropTypes.string,
-        country: PropTypes.string,
-    }).isRequired,
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
 };
 
 export default App;
--- weather-app/src/index.js
+++ weather-app/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/app';
+import App from './App';
 import { location } from './data/forecast.json';
 
-render( < App location = { location }
-        />, document.getElementById('root'));
+render(
+  <App location={location} />, document.getElementById('root')
+);
